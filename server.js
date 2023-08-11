cc49d01bbafe5f77eff2be66975d70332a7fa616
muskan-dangol <dangolmuskan19@gmail.com>
2023-08-11 16:12:48 +0300

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next){ //adding middleware to express
+        console.log('Private route hit!');
+        next();
+    }, 
+    logger: function(req, res, next){
+        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+    res.send('This page is about me!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+    console.log('Express Server started on port ' + PORT);
+});
+
+C:\Users\MSI\Desktop\Projects\Node_course\web_server\Public\index.html
